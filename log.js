fdf8dadbb5c372f4b970aedc86471d91b1385f93
Jacob Davis <jsdavis1010@gmail.com>
2017-03-17 16:33:33 -0700

    Added fault tolerance to logger intialization

--- log.js
+++ log.js
@@ -2,15 +2,26 @@
 
 const bunyan = require('bunyan');
 const path = require('path');
+const fs = require('fs');
 
 const env = process.env.NODE_ENV;
+const silent = bunyan.FATAL + 1;
 
 // Disable stdout if not in dev
-const stdoutLevel = env === 'dev' ? 'info' : bunyan.FATAL + 1;
+const stdoutLevel = env === 'dev' ? 'info' : silent;
 
 // Disable error log if in test
-const errLevel = env === 'test' ? bunyan.FATAL + 1 : 'error';
+const errLevel = env === 'test' ? silent : 'error';
 
+// Create the logs directory if it doesn't already exist
+try {
+  fs.mkdirSync(path.join(__dirname, 'logs'));
+}
+catch (e) {
+  if (e.code != 'EEXIST') throw e;
+}
+
+// Create the logger
 const log = bunyan.createLogger({
   name: 'app',
   serializers: bunyan.stdSerializers,
