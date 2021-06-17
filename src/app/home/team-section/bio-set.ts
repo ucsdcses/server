880c5ab9bb55ecb0ef7aa713e3125186d445c943
Yuzi Lyu <yul134@ucsd.edu>
2021-06-17 09:39:46 -0700

    new  board pics added

--- src/app/home/team-section/bio-set.ts
+++ src/app/home/team-section/bio-set.ts
@@ -9,44 +9,80 @@ class BioSet {
         // parent class type and casts it to a regular Bio object.
         new Bio (
             {
-                name: 'Carleen Li',
-                title: 'Co-President',
+                name: 'Yuzi Lyu',
+                title: 'President',
                 links: {
-                    mailToUrl: 'mailto:cyli@ucsd.edu',
-                    linkedinUrl: 'https://www.linkedin.com/in/carleen-li/',
-                    githubUrl: 'https://github.com/carleenli',
-                    imageUrl: 'assets/images/board-2020/Carleen.jpg'
+                    mailToUrl: 'mailto:yul134@ucsd.edu',
+                    linkedinUrl: 'https://www.linkedin.com/in/yuzi-lyu/',
+                    githubUrl: 'https://github.com/yuzilyu',
+                    imageUrl: 'assets/images/board-2021/YuziLyu.jpg'
                 }
             }
         ),
         new Bio (
             {
-              name: 'Chen Meng',
-              title: 'Co-President',
+              name: 'Anshul Singh',
+              title: 'VP-External',
               links: {
-                  mailToUrl: 'mailto:c5meng@ucsd.edu',
-                  linkedinUrl: 'https://www.linkedin.com/in/chenmeng98/',
-                  githubUrl: 'https://github.com/MCharming98',
-                  imageUrl: 'assets/images/board-2020/Chen.jpg'
+                mailToUrl: 'mailto:a2singh@ucsd.edu',
+                linkedinUrl: 'https://www.linkedin.com/in/anshul-singh4/',
+                githubUrl: 'https://github.com/anshulsinghh',
+                imageUrl: 'assets/images/board-2020/Anshul.jpg'
               }
             }
       ),
         new Bio (
             {
-                name: 'Pawan Paleja',
-                title: 'VP External',
+                name: 'Ke Xu',
+                title: 'VP Internal',
                 links: {
-                  mailToUrl: 'mailto:ppaleja@ucsd.edu',
-                  linkedinUrl: 'https://www.linkedin.com/in/pawanpaleja',
-                  githubUrl: 'https://github.com/ppaleja',
-                  imageUrl: 'assets/images/board-2020/Pawan.jpg'
+                  mailToUrl: 'mailto:k6xu@ucsd.edu',
+                  linkedinUrl: 'https://www.linkedin.com/in/ke-xu-079351211/',
+                  githubUrl: 'https://github.com/lorraineeeee',
+                  imageUrl: 'assets/images/board-2021/Ke.jpg'
+                }
+            }
+        ),
+        new Bio (
+            {
+                name: 'Dennis Luc',
+                title: 'VP Finance',
+                links: {
+                    mailToUrl: 'mailto:dluc@ucsd.edu',
+                    linkedinUrl: 'https://www.linkedin.com/in/dennisluc/',
+                    githubUrl: 'https://github.com/denniskluc',
+                    imageUrl: 'assets/images/board-2020/Dennis.jpg'
+                }
+            }
+        ),
+        new Bio (
+            {
+                name: 'Deepansha Singh',
+                title: 'Pro-Dev Chair',
+                links: {
+                  mailToUrl: 'mailto:d1singh@ucsd.edu',
+                  linkedinUrl: 'https://www.linkedin.com/in/deepansha-singh/',
+                  githubUrl: 'https://github.com/Algorithmist-Girl',
+                  imageUrl: 'assets/images/board-2021/Deepansha.jpg'
+                }
+            }
+        ),
+        new Bio (
+            {
+                name: 'Tony Tang',
+                title: 'Pro-Dev Chair',
+                links: {
+                  mailToUrl: 'mailto:zit001@ucsd.edu',
+                  linkedinUrl: 'https://www.linkedin.com/in/zixuan-tony-tang',
+                  githubUrl: 'https://github.com/TonyTang2001',
+                  imageUrl: 'assets/images/board-2021/TonyTang.jpg'
                 }
             }
         ),
         new Bio (
             {
                 name: 'Nour Yehia',
-                title: 'VP Internal',
+                title: 'Projects Chair',
                 links: {
                   mailToUrl: 'mailto:nyehia@ucsd.edu',
                   linkedinUrl: 'https://www.linkedin.com/in/nyehia/',
@@ -57,86 +93,25 @@ class BioSet {
         ),
         new Bio (
             {
-                name: 'Tae Kwang(Jason) Chung',
-                title: 'VP Finance',
-                links: {
-                  mailToUrl: 'mailto:tkc016@ucsd.edu',
-                  linkedinUrl: 'https://www.linkedin.com/in/tae-kwang-jason-chung/',
-                  githubUrl: 'https://github.com/jatotheson',
-                  imageUrl: 'assets/images/board-2020/Jason.jpg'
-                }
-            }
-        ),
-        new Bio (
-            {
-                name: 'Yuling Shi',
-                title: 'Pro-Dev Chair',
-                links: {
-                  mailToUrl: 'mailto:yus252@ucsd.edu',
-                  linkedinUrl: 'https://www.linkedin.com/in/yulingshi/',
-                  githubUrl: 'https://github.com/yus252',
-                  imageUrl: 'assets/images/board-2020/Yuling.jpg'
-                }
-            }
-        ),
-        new Bio (
-            {
-                name: 'Eric Kang',
-                title: 'Pro-Dev Chair',
-                links: {
-                  mailToUrl: 'mailto:ekang@ucsd.edu',
-                  linkedinUrl: 'https://www.linkedin.com/in/eric-kang-3589b1144/',
-                  githubUrl: 'https://github.com/eric-h-kang',
-                  imageUrl: 'assets/images/board-2020/Eric.jpg'
-                }
-            }
-        ),
-        new Bio (
-            {
-                name: 'Yuzi LYu',
+                name: 'Stephen Boussarov',
                 title: 'Projects Chair',
                 links: {
-                  mailToUrl: 'mailto:yul134@ucsd.edu',
-                  linkedinUrl: 'https://www.linkedin.com/in/yuzi-lyu-13658a1a3/',
-                  githubUrl: 'https://github.com/yuzilyu',
-                  imageUrl: 'assets/images/board-2020/Yuzi.jpg'
+                  mailToUrl: 'mailto:sboussar@ucsd.edu',
+                  linkedinUrl: 'https://www.linkedin.com/in/stephen-boussarov/',
+                  githubUrl: 'https://github.com/stephenbusi',
+                  imageUrl: 'assets/images/404.jpg'
                 }
             }
         ),
         new Bio (
             {
-                name: 'Anshul Singh',
-                title: 'Projects Chair',
-                links: {
-                  mailToUrl: 'mailto:a2singh@ucsd.edu',
-                  linkedinUrl: 'https://www.linkedin.com/in/anshul-singh4/',
-                  githubUrl: 'https://github.com/anshulsinghh',
-                  imageUrl: 'assets/images/board-2020/Anshul.jpg'
-                }
-            }
-        ),
-        new Bio (
-            {
-                name: 'Eunice Lin',
-                title: 'Design Chair',
-                links: {
-                 
-                  mailToUrl: 'mailto:silin@ucsd.edu',
-                  linkedinUrl: 'http://linkedin.com/in/eunice-lin-54aa921a2',
-                  githubUrl: '',
-                  imageUrl: 'assets/images/board-2020/Eunice.jpg'
-                }
-            }
-        ),
-        new Bio (
-            {
-                name: 'Dennis Luc',
+                name: 'Tanay Reddy',
                 title: 'PR Chair',
                 links: {
-                  mailToUrl: 'mailto:dluc@ucsd.edu',
-                  linkedinUrl: 'https://www.linkedin.com/in/dennisluc/',
-                  githubUrl: 'https://github.com/denniskluc',
-                  imageUrl: 'assets/images/board-2020/Dennis.jpg'
+                  mailToUrl: 'mailto:tanaykreddy@gmail.com',
+                  linkedinUrl: 'https://www.linkedin.com/in/tanay-reddy-a281411b5/',
+                  githubUrl: 'https://github.com/tannaykreddy',
+                  imageUrl: 'assets/images/board-2021/Tanay.jpg'
                 }
             }
         ),
