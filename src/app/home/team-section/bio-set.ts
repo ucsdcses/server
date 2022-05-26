7f845a47867f69b9134956a0c639532d9b25fbd0
yuzilyu <yul134@ucsd.edu>
2022-05-25 20:30:20 -0700

    changed order

--- src/app/home/team-section/bio-set.ts
+++ src/app/home/team-section/bio-set.ts
@@ -7,18 +7,7 @@ class BioSet {
     public static BIO_SET: Array<Bio> = [
         // Work around to get the getImage when serializing from JSON. Creates a
         // parent class type and casts it to a regular Bio object.
-        new Bio (
-            {
-                name: 'Ke Xu',
-                title: 'VP Internal',
-                links: {
-                  mailToUrl: 'mailto:k6xu@ucsd.edu',
-                  linkedinUrl: 'https://www.linkedin.com/in/ke-xu-079351211/',
-                  githubUrl: 'https://github.com/lorraineeeee',
-                  imageUrl: 'assets/images/board-2021/Ke.jpg'
-                }
-            }
-        ),
+        
         new Bio (
             {
                 name: 'Deepansha Singh',
@@ -43,6 +32,18 @@ class BioSet {
                 }
             }
         ),
+        new Bio (
+            {
+                name: 'Ke Xu',
+                title: 'VP Internal',
+                links: {
+                  mailToUrl: 'mailto:k6xu@ucsd.edu',
+                  linkedinUrl: 'https://www.linkedin.com/in/ke-xu-079351211/',
+                  githubUrl: 'https://github.com/lorraineeeee',
+                  imageUrl: 'assets/images/board-2021/Ke.jpg'
+                }
+            }
+        )
     ];
 }
 
