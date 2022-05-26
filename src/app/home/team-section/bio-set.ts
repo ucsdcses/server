b3c8a6e8013b36afaf1da9dfcbfa5a31e90566b0
yuzilyu <yul134@ucsd.edu>
2022-05-25 20:22:34 -0700

    removed graduating board members

--- src/app/home/team-section/bio-set.ts
+++ src/app/home/team-section/bio-set.ts
@@ -9,30 +9,6 @@ class BioSet {
         // parent class type and casts it to a regular Bio object.
         new Bio (
             {
-                name: 'Yuzi Lyu',
-                title: 'President',
-                links: {
-                    mailToUrl: 'mailto:yul134@ucsd.edu',
-                    linkedinUrl: 'https://www.linkedin.com/in/yuzi-lyu/',
-                    githubUrl: 'https://github.com/yuzilyu',
-                    imageUrl: 'assets/images/board-2021/YuziLyu.jpg'
-                }
-            }
-        ),
-        new Bio (
-            {
-              name: 'Anshul Singh',
-              title: 'VP-External',
-              links: {
-                mailToUrl: 'mailto:a2singh@ucsd.edu',
-                linkedinUrl: 'https://www.linkedin.com/in/anshul-singh4/',
-                githubUrl: 'https://github.com/anshulsinghh',
-                imageUrl: 'assets/images/board-2020/Anshul.jpg'
-              }
-            }
-      ),
-        new Bio (
-            {
                 name: 'Ke Xu',
                 title: 'VP Internal',
                 links: {
@@ -45,20 +21,8 @@ class BioSet {
         ),
         new Bio (
             {
-                name: 'Dennis Luc',
-                title: 'VP Finance',
-                links: {
-                    mailToUrl: 'mailto:dluc@ucsd.edu',
-                    linkedinUrl: 'https://www.linkedin.com/in/dennisluc/',
-                    githubUrl: 'https://github.com/denniskluc',
-                    imageUrl: 'assets/images/board-2020/Dennis.jpg'
-                }
-            }
-        ),
-        new Bio (
-            {
                 name: 'Deepansha Singh',
-                title: 'Pro-Dev Chair',
+                title: 'Co-President',
                 links: {
                   mailToUrl: 'mailto:d1singh@ucsd.edu',
                   linkedinUrl: 'https://www.linkedin.com/in/deepansha-singh/',
@@ -69,44 +33,8 @@ class BioSet {
         ),
         new Bio (
             {
-                name: 'Tony Tang',
-                title: 'Pro-Dev Chair',
-                links: {
-                  mailToUrl: 'mailto:zit001@ucsd.edu',
-                  linkedinUrl: 'https://www.linkedin.com/in/zixuan-tony-tang',
-                  githubUrl: 'https://github.com/TonyTang2001',
-                  imageUrl: 'assets/images/board-2021/TonyTang.JPG'
-                }
-            }
-        ),
-        new Bio (
-            {
-                name: 'Nour Yehia',
-                title: 'Projects Chair',
-                links: {
-                  mailToUrl: 'mailto:nyehia@ucsd.edu',
-                  linkedinUrl: 'https://www.linkedin.com/in/nyehia/',
-                  githubUrl: 'https://github.com/nouryehia',
-                  imageUrl: 'assets/images/board-2020/Nour.jpg'
-                }
-            }
-        ),
-        new Bio (
-            {
-                name: 'Stephen Boussarov',
-                title: 'Projects Chair',
-                links: {
-                  mailToUrl: 'mailto:sboussar@ucsd.edu',
-                  linkedinUrl: 'https://www.linkedin.com/in/stephen-boussarov/',
-                  githubUrl: 'https://github.com/stephenbusi',
-                  imageUrl: 'assets/images/board-2021/Stephen.jpg'
-                }
-            }
-        ),
-        new Bio (
-            {
                 name: 'Vardhan Agarwal',
-                title: 'PR Chair',
+                title: 'Co-President',
                 links: {
                   mailToUrl: 'mailto:v7agarwa@ucsd.edu',
                   linkedinUrl: 'https://www.linkedin.com/in/vardhanagarwal/',
