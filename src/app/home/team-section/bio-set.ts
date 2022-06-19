8137a8fa9d7a1a21d67b1e9e762d86eaa22168f6
Vardhan Agarwal <v7agarwa@ucsd.edu>
2022-06-19 18:35:19 -0400

    minor fixes

--- src/app/home/team-section/bio-set.ts
+++ src/app/home/team-section/bio-set.ts
@@ -7,7 +7,18 @@ class BioSet {
     public static BIO_SET: Array<Bio> = [
         // Work around to get the getImage when serializing from JSON. Creates a
         // parent class type and casts it to a regular Bio object.
-        
+        new Bio (
+            {
+                name: 'Vardhan Agarwal',
+                title: 'Co-President',
+                links: {
+                  mailToUrl: 'mailto:v7agarwa@ucsd.edu',
+                  linkedinUrl: 'https://www.linkedin.com/in/vardhanagarwal/',
+                  githubUrl: 'https://github.com/vardhan08',
+                  imageUrl: 'assets/images/board-2022/Vardhan_Agarwal.jpg'
+                }
+            }
+        ),
         new Bio (
             {
                 name: 'Deepansha Singh',
@@ -16,19 +27,7 @@ class BioSet {
                   mailToUrl: 'mailto:d1singh@ucsd.edu',
                   linkedinUrl: 'https://www.linkedin.com/in/deepansha-singh/',
                   githubUrl: 'https://github.com/Algorithmist-Girl',
-                  imageUrl: 'assets/images/board-2021/Deepansha.jpg'
-                }
-            }
-        ),
-        new Bio (
-            {
-                name: 'Vardhan Agarwal',
-                title: 'Co-President',
-                links: {
-                  mailToUrl: 'mailto:v7agarwa@ucsd.edu',
-                  linkedinUrl: 'https://www.linkedin.com/in/vardhanagarwal/',
-                  githubUrl: '',
-                  imageUrl: 'assets/images/board-2021/VardhanAgarwal.jpg'
+                  imageUrl: 'assets/images/board-2022/Deepansha.jpg'
                 }
             }
         ),
@@ -40,7 +39,7 @@ class BioSet {
                   mailToUrl: 'mailto:k6xu@ucsd.edu',
                   linkedinUrl: 'https://www.linkedin.com/in/ke-xu-079351211/',
                   githubUrl: 'https://github.com/lorraineeeee',
-                  imageUrl: 'assets/images/board-2021/Ke.jpg'
+                  imageUrl: 'assets/images/board-2022/Ke.jpg'
                 }
             }
         )
