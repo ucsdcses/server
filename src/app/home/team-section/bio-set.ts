024d493896ad5c0a294faf49e8ddfcdd185ba18b
Vardhan Agarwal <v7agarwa@ucsd.edu>
2022-07-03 23:17:12 -0700

    updated board members

--- src/app/home/team-section/bio-set.ts
+++ src/app/home/team-section/bio-set.ts
@@ -10,7 +10,7 @@ class BioSet {
         new Bio (
             {
                 name: 'Vardhan Agarwal',
-                title: 'Co-President',
+                title: 'President, VP Finance',
                 links: {
                   mailToUrl: 'mailto:v7agarwa@ucsd.edu',
                   linkedinUrl: 'https://www.linkedin.com/in/vardhanagarwal/',
@@ -22,12 +22,24 @@ class BioSet {
         new Bio (
             {
                 name: 'Deepansha Singh',
-                title: 'Co-President',
+                title: 'President',
                 links: {
                   mailToUrl: 'mailto:d1singh@ucsd.edu',
                   linkedinUrl: 'https://www.linkedin.com/in/deepansha-singh/',
                   githubUrl: 'https://github.com/Algorithmist-Girl',
                   imageUrl: 'assets/images/board-2022/Deepansha.jpg'
+                }
+            }
+        ),
+        new Bio (
+            {
+                name: 'Amit Namburi',
+                title: 'VP Internal',
+                links: {
+                  mailToUrl: 'mailto:anamburi@ucsd.edu',
+                  linkedinUrl: 'https://www.linkedin.com/in/amit-namburi/',
+                  githubUrl: 'https://github.com/namburiamit/',
+                  imageUrl: 'assets/images/board-2022/Amit.jpg'
                 }
             }
         ),
@@ -40,6 +52,54 @@ class BioSet {
                   linkedinUrl: 'https://www.linkedin.com/in/ke-xu-079351211/',
                   githubUrl: 'https://github.com/lorraineeeee',
                   imageUrl: 'assets/images/board-2022/Ke.jpg'
+                }
+            }
+        ),
+        new Bio (
+            {
+                name: 'Gabriel Leif Diaz',
+                title: 'Projects Chair',
+                links: {
+                  mailToUrl: 'mailto:lgdiaz@ucsd.edu',
+                  linkedinUrl: 'https://www.linkedin.com/in/gabriel-diaz-57815118b/',
+                  githubUrl: 'https://github.com/gabriel-diaz-ucsd',
+                  imageUrl: 'assets/images/board-2022/Gabriel.jpg'
+                }
+            }
+        ),
+        new Bio (
+            {
+                name: 'Saman Khadivar',
+                title: 'Pro-Dev Chair',
+                links: {
+                  mailToUrl: 'mailto:skhadivar@ucsd.edu',
+                  linkedinUrl: 'https://www.linkedin.com/in/samankhadivar/',
+                  githubUrl: 'https://github.com/ucsdcses/',
+                  imageUrl: 'assets/images/board-2022/Saman.jpg'
+                }
+            }
+        ),
+        new Bio (
+            {
+                name: 'Saatvhik Dirisala',
+                title: 'Pro-Dev Chair',
+                links: {
+                  mailToUrl: 'mailto:sdirisala@ucsd.edu',
+                  linkedinUrl: 'https://www.linkedin.com/mwlite/in/saathvik-dirisala-4a1817197',
+                  githubUrl: 'https://github.com/saathvikpd',
+                  imageUrl: 'assets/images/board-2022/Saathvik.jpg'
+                }
+            }
+        ),
+        new Bio (
+            {
+                name: 'Varun Parekh',
+                title: 'PR Chair',
+                links: {
+                  mailToUrl: 'mailto:vparekh@ucsd.edu',
+                  linkedinUrl: 'https://www.linkedin.com/in/varun-devang-parekh-223627212',
+                  githubUrl: 'https://github.com/ucsdcses/',
+                  imageUrl: 'assets/images/board-2022/Varun.jpg'
                 }
             }
         )
